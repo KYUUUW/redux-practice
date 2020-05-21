2708a3e6453464fa6db6e759a36eac6d49b38ded
kyuwon <kyuwon@kyuwonui-MacBookPro.local>
2020-05-22 00:35:54 +0900

    🎉 Initial commit

---
+++ vanilla-redux/src/index.js
@@ -0,0 +1,25 @@
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.getElementById("number");
+
+let count = 0;
+number.innerText = count;
+
+const updateText = () => {
+    number.innerText = count;
+}
+
+const handleAdd = () => {
+    count = count + 1;
+    //console.log("add", count);
+    updateText();
+}
+
+const handleMinus = () => {
+    count = count - 1;
+    //console.log("minus", count);
+    updateText();
+}
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
